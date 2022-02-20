e8e8a8b1f0391b3abd0dec22c6f04b763bc4858e
Muhammad Abdurrasyid <55310844+RASYD@users.noreply.github.com>
2022-02-20 17:53:31 +0700

    make migration and login controller

--- database/migrations/2022_02_19_085551_create_sub_bab_table.php
+++ database/migrations/2022_02_19_085551_create_sub_bab_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateSubBabTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sub_bab', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('jk');
-            $table->string('bidang');
-            $table->string('status');
-            $table->string('username');
-            $table->string('password');
+            $table->bigInteger('bab_id');
+            $table->string('nama_bab');
             $table->timestamps();
         });
     }
@@ -32,6 +28,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sub_bab');
     }
 }
--- database/migrations/2022_02_19_085606_create_question_table.php
+++ database/migrations/2022_02_19_085606_create_question_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateQuestionTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('question', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('jk');
-            $table->string('bidang');
-            $table->string('status');
-            $table->string('username');
-            $table->string('password');
+            $table->bigInteger('sub_bab_id');
+            $table->integer('nomor');
+            $table->string('question');
             $table->timestamps();
         });
     }
@@ -32,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('question');
     }
 }
