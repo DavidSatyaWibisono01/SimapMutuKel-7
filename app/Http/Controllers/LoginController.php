87b47ed4a4aa21dc140716ca7435fb300b096515
Muhammad Abdurrasyid <55310844+RASYD@users.noreply.github.com>
2022-02-22 19:41:13 +0700

    menyelesaikan login dan pembuatan datausercontroller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Auth\Events\Validated;
 
 class LoginController extends Controller
 {
@@ -17,5 +19,33 @@ class LoginController extends Controller
         return view('login');
     }
 
+    public function login(request $request){
+        request()->validate(
+            [
+                'username' => 'required',
+                'password' => 'required',
+            ]);
 
+        $credential = $request->only('username','password');
+            if (Auth::attempt($credential)) {
+                $user = Auth::user();
+                if ($user->level == 'admin') {
+                    return redirect()->intended('/dashboard');
+                } elseif ($user->level == 'user') {
+                    return redirect()->intended('/user-dashboard');
+                }
+                return redirect()->intended('/');
+            }
+
+        return redirect('/')
+                                ->withInput()
+                                ->withErrors(['login_gagal' => 'These credentials do not match our records.']);
+    }
+
+    public function logout(Request $request)
+    {
+       $request->session()->flush();
+       Auth::logout();
+       return Redirect('login');
+    }
 }
